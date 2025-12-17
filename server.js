444b91a04161f93663fbf441c7fda5051a565d8b
Ajay Pawar <pawarajay7.ap@gmail.com>
2025-12-17 14:21:28 +0530

    update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const projectRoutes = require("./routes/projects");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "https://crm-frontend-delta-two.vercel.app";
 
 
 app.use(
